0c97b4a4915b20705b7d29ae662faa0ad9d54c4a
Osa-Kosa123 <supremeoskar@gmail.com>
2025-11-25 11:04:55 +0100

    Add files via upload

---
+++ TABD/Fresh Wear/src/components/header.php
@@ -0,0 +1,14 @@
+        <div id="logoBox">
+            <img src="../assets/logo.png" alt="logo" id="logo">
+        </div>
+        <div id="searchBox">
+            <form action="search.php" method="post" id="searchForm">
+                <input type="text" name="search" id="searchField" placeholder="Szukaj produktów...">
+                <button type="submit" id="searchButton">
+                    <img src="../assets/search.png" alt="search icon" id="searchIcon">
+                </button>
+            </form>
+        </div>
+        <div id="buttonBox">
+            <button id="cart"></button>
+        </div>
